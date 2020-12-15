c7de0c921007c326b7fb8d001b0533111e452839
miridium-electrode <checkerdance@yahoo.com>
2020-12-15 22:34:54 +0700

    add function to extract input

--- web/src/pages/LoginPage.tsx
+++ web/src/pages/LoginPage.tsx
@@ -1,21 +1,28 @@
 import React from "react";
 
+function getInputField() {
+    let foo: string = document.getElementsByTagName('input')[0].value;
+    let foo1: string = document.getElementsByTagName('input')[1].value;
+    console.log(foo);
+    console.log(foo1);
+}
+
 function LoginPage() {
     return (
-        <div className="flex justify-center items-center h-screen">
-            <div className="flex flex-col justify-center items-center">
-                <h1 className="text-white">Login</h1>
-                <div className="flex flex-col justify-center items-center">
-                    <label htmlFor="uname">Username</label>
-                    <input type="text" name="uname"/>
+        <div className="flex justify-center items-center h-screen bg-indigo-900">
+            <div className="flex flex-col justify-center items-center bg-indigo-700 p-8">
+                <h1 className="text-white text-3xl m-4">Login</h1>
+                <div className="flex flex-col justify-center items-center mb-4">
+                    <label htmlFor="uname" className="text-white">Username</label>
+                    <input type="text" name="uname" className="bg-indigo-500" />
                 </div>
-                <div className="flex flex-col justify-center items-center">
-                    <label htmlFor="pwd">Password</label>
-                    <input type="password" name="pwd"/>
+                <div className="flex flex-col justify-center items-center mb-4">
+                    <label htmlFor="pwd" className="text-white">Password</label>
+                    <input type="password" name="pwd" className="bg-indigo-500" />
                 </div>
-                <button>Login</button>
-                <a href="#">Forgot password?</a>
-                <a href="#">Not a user?</a>
+                <button className="text-white bg-green-400 py-2 px-20 mb-2" onClick={getInputField}>Login</button>
+                <a href="#" className="text-white mb-2">Forgot password?</a>
+                <a href="#" className="text-white mb-2">Not a user?</a>
             </div>
         </div>
     );
