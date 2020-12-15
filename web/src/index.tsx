d4bf71175c68ecbb36bb7e7ad1bb0ccca32208bb
miridium-electrode <checkerdance@yahoo.com>
2020-12-15 09:38:10 +0700

    added loginpage to index

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -2,9 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
+import LoginPage from "./pages/LoginPage";
 
 ReactDOM.render(
   <React.StrictMode>
+    <LoginPage/>
   </React.StrictMode>,
   document.getElementById('root')
 );
