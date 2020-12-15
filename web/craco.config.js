cf603e65d07fd473f5dff0a2cff400a5b6668681
miridium-electrode <checkerdance@yahoo.com>
2020-12-15 09:17:04 +0700

    🔧 add craco configuration

---
+++ web/craco.config.js
@@ -0,0 +1,11 @@
+// craco config, some sort of middleware between tailwindcss and react
+module.exports = {
+    style: {
+        postcss: {
+            plugins: [
+                require('tailwindcss'),
+                require('autoprefixer'),
+            ],
+        },
+    },
+}
