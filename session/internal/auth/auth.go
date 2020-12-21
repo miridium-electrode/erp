06489eb962cf884bb8613e2716ea5a4ee717c835
miridium-electrode <checkerdance@yahoo.com>
2020-12-21 10:07:28 +0700

    authenticating and return jwt work

--- session/internal/auth/auth.go
+++ session/internal/auth/auth.go
@@ -3,9 +3,22 @@ package auth
 import (
 	"net/http"
 	"encoding/json"
+	"time"
 	"miridium_electrode/session/internal/dbconn"
+	jwt "github.com/dgrijalva/jwt-go"
 )
 
+type M map[string]interface{}
+
+type MyClaim struct {
+	jwt.StandardClaims
+	Username string `json:"username"`
+}
+
+var APPLICATION_NAME = "session of erp"
+var LOGIN_EXPIRATION_DURATION = time.Duration(1) * time.Hour
+var JWT_SIGNING_METHOD = jwt.SigningMethodHS256
+var JWT_SIGNATURE_KEY = []byte("the secret of kalimdor")
 
 
 func Login(w http.ResponseWriter, r *http.Request) {
@@ -21,19 +34,51 @@ func Login(w http.ResponseWriter, r *http.Request) {
 			return
 		}
 
-		w.Header().Set("Content-type", "application/json")
-		response := struct{
-			Username string `json:"username"`
-			Password string `json:"password"`
-		} {}
-		dbconn.Startconn().Table("logins").Select("username", "password").Where("username = ? AND password = ?", payload.Username, payload.Password).Scan(&response)
-		reserror := json.NewEncoder(w).Encode(response)
-		if reserror != nil {
-			http.Error(w, reserror.Error(), http.StatusInternalServerError)
+		userDontExist := authenticateUser(payload.Username, payload.Password)
+		if userDontExist == true {
+			http.Error(w, "invalid username or password", http.StatusBadRequest)
+			return
+		}
+
+		claims := MyClaim {
+			StandardClaims: jwt.StandardClaims{
+				Issuer:    APPLICATION_NAME,
+				ExpiresAt: time.Now().Add(LOGIN_EXPIRATION_DURATION).Unix(),
+			},
+			Username: payload.Username,
+		}
+
+		token := jwt.NewWithClaims(
+			JWT_SIGNING_METHOD,
+			claims,
+		)
+
+		signedToken, tokerr := token.SignedString(JWT_SIGNATURE_KEY)
+		if tokerr != nil {
+			http.Error(w, tokerr.Error(), http.StatusBadRequest)
+			return
+		}
+
+		reserr := json.NewEncoder(w).Encode(M{"token": signedToken})
+		if reserr != nil {
+			http.Error(w, reserr.Error(), http.StatusBadRequest)
 			return
 		}
 	default:
-		http.Error(w, "", http.StatusBadRequest)
+		http.Error(w, "unsupported http method", http.StatusBadRequest)
 	}
+}
+
+func authenticateUser(u, p string) (bool) {
+	res := struct{
+		username string
+		password string
+	}{}
+	dbres := dbconn.Startconn().Table("logins").Select("username", "password").Where("username = ? AND password = ?", u, p).Scan(&res)
+	if dbres.RowsAffected != 1 {
+		return true
+	}
+
+	return false
 }
 
