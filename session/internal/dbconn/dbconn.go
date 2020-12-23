46cc94e480f59eb98410ca37d55ee57091c47e05
miridium-electrode <checkerdance@yahoo.com>
2020-12-23 14:01:29 +0700

    add docs

--- session/internal/dbconn/dbconn.go
+++ session/internal/dbconn/dbconn.go
@@ -5,6 +5,7 @@ import (
 	"gorm.io/driver/sqlite"
 )
 
+// open database connection and return a database connection
 func Startconn() *gorm.DB {
 	db, err := gorm.Open(sqlite.Open("/home/arfe/arfchive/personalliking/learn_to_dev/big-project/erp/session/login.db"), &gorm.Config{})
 	if err != nil {
