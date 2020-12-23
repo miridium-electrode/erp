0b94e3dcc3abaa12b2ab84e540abf11a2dc1f564
miridium-electrode <checkerdance@yahoo.com>
2020-12-23 13:59:41 +0700

    added cors condition, refactor authenticate user, add mini docs

--- session/internal/auth/auth.go
+++ session/internal/auth/auth.go
@@ -8,8 +8,10 @@ import (
 	jwt "github.com/dgrijalva/jwt-go"
 )
 
+// store jwt token and then converted to json
 type M map[string]interface{}
 
+// Representation of jwt data
 type MyClaim struct {
 	jwt.StandardClaims
 	Username string `json:"username"`
@@ -20,55 +22,63 @@ var LOGIN_EXPIRATION_DURATION = time.Duration(1) * time.Hour
 var JWT_SIGNING_METHOD = jwt.SigningMethodHS256
 var JWT_SIGNATURE_KEY = []byte("the secret of kalimdor")
 
-
+// Accept post request of username and password, and then return a cookie of json web token
 func Login(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "OPTIONS, POST")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Origin, Accept, token")
 	switch r.Method {
-	case "POST":
-		decoder := json.NewDecoder(r.Body)
-		payload := struct{
-			Username string `json:"username"`
-			Password string `json:"password"`
-		}{}
-		if err := decoder.Decode(&payload); err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
+	  case "OPTIONS":
+		  w.Write([]byte("allowed"))
+		  return
+	  case "POST":
+		  decoder := json.NewDecoder(r.Body)
+		  payload := struct{
+			  Username string `json:"username"`
+			  Password string `json:"password"`
+		  }{}
+		  if err := decoder.Decode(&payload); err != nil {
+			  http.Error(w, err.Error(), http.StatusInternalServerError)
+			  return
+		  }
 
-		userDontExist := authenticateUser(payload.Username, payload.Password)
-		if userDontExist == true {
-			http.Error(w, "invalid username or password", http.StatusBadRequest)
-			return
-		}
+		  ok := authenticateUser(payload.Username, payload.Password)
+		  if !ok == true {
+			  http.Error(w, "invalid username or password", http.StatusBadRequest)
+			  return
+		  }
 
-		claims := MyClaim {
-			StandardClaims: jwt.StandardClaims{
-				Issuer:    APPLICATION_NAME,
-				ExpiresAt: time.Now().Add(LOGIN_EXPIRATION_DURATION).Unix(),
-			},
-			Username: payload.Username,
-		}
+		  claims := MyClaim {
+			  StandardClaims: jwt.StandardClaims{
+				  Issuer:    APPLICATION_NAME,
+				  ExpiresAt: time.Now().Add(LOGIN_EXPIRATION_DURATION).Unix(),
+			  },
+			  Username: payload.Username,
+		  }
 
-		token := jwt.NewWithClaims(
-			JWT_SIGNING_METHOD,
-			claims,
-		)
+		  token := jwt.NewWithClaims(
+			  JWT_SIGNING_METHOD,
+			  claims,
+		  )
 
-		signedToken, tokerr := token.SignedString(JWT_SIGNATURE_KEY)
-		if tokerr != nil {
-			http.Error(w, tokerr.Error(), http.StatusBadRequest)
-			return
-		}
+		  signedToken, tokerr := token.SignedString(JWT_SIGNATURE_KEY)
+		  if tokerr != nil {
+			  http.Error(w, tokerr.Error(), http.StatusBadRequest)
+			  return
+		  }
 
-		reserr := json.NewEncoder(w).Encode(M{"token": signedToken})
-		if reserr != nil {
-			http.Error(w, reserr.Error(), http.StatusBadRequest)
-			return
-		}
-	default:
-		http.Error(w, "unsupported http method", http.StatusBadRequest)
+		  reserr := json.NewEncoder(w).Encode(M{"token": signedToken})
+		  if reserr != nil {
+			  http.Error(w, reserr.Error(), http.StatusBadRequest)
+			  return
+		  }
+	  default:
+		  http.Error(w, "unsupported http method", http.StatusBadRequest)
 	}
 }
 
+// take username and password from request body and than compare it to database
+// return false if username and password is invalid
 func authenticateUser(u, p string) (bool) {
 	res := struct{
 		username string
@@ -76,9 +86,9 @@ func authenticateUser(u, p string) (bool) {
 	}{}
 	dbres := dbconn.Startconn().Table("logins").Select("username", "password").Where("username = ? AND password = ?", u, p).Scan(&res)
 	if dbres.RowsAffected != 1 {
-		return true
+		return false
 	}
 
-	return false
+	return true
 }
 
