c2f461e5c8677fccd8e4db0b59162b6abded0f08
miridium-electrode <checkerdance@yahoo.com>
2020-12-25 06:03:04 +0700

    container work

---
+++ web/src/pages/AbsenPage.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Background from '../components/Background';
+
+function AbsenPage() {
+    return (
+        <Background>
+            <div className="container bg-indigo-500 mx-auto absolute w-9/10 h-9/10" >
+                
+            </div>
+        </Background>
+    );
+}
+
+export default AbsenPage;
