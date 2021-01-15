496295d3ce1c29039f0a3341f4969cfabcb83ba3
miridium-electrode <checkerdance@yahoo.com>
2021-01-15 17:50:39 +0700

    user settings almost complete

---
+++ web/src/pages/user-settings-page/Usersettings.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import Background from '../../components/background/Background';
+import Navbar from '../../components/navbar/Navbar';
+import Container from '../../components/container/Container';
+import Usb1 from '../../components/user-settings-box-1/Usb1';
+import './style.scss';
+
+function Usersettings() {
+    return (
+        <Background forPage="other">
+            <Navbar/>
+            <Container forPage="usersettings">
+                <Usb1/>
+                <div className="box2diff">
+                    <div className="field">
+                        <label htmlFor="uname">Username</label>
+                        <input name="uname" type="input"/>
+                    </div>
+                    <div className="field">
+                        <label htmlFor="pwd">Password</label>
+                        <input name="pwd" type="input"/>
+                    </div>
+                    <div className="field">
+                        <label htmlFor="repwd">Retype Password</label>
+                        <input type="text" name="repwd"/>
+                    </div>
+                    <div className="twobtn">
+                        <button>Delete account</button>
+                        <button>Update</button>
+                    </div>
+                </div>
+            </Container>
+        </Background>
+    );
+}
+
+export default Usersettings;
