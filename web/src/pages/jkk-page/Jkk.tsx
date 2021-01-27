cc1925c366e5171f8bf8bb5059d077ac7bd6c0c1
miridium-electrode <checkerdance@yahoo.com>
2021-01-27 19:08:26 +0700

    added jadwal kerja karyawan page

---
+++ web/src/pages/jkk-page/Jkk.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import Background from '../../components/background/Background';
+import Navbar from '../../components/navbar/Navbar';
+import Container from '../../components/container/Container';
+import './style.scss';
+
+function Jkk() {
+    return (
+        <Background forPage="other">
+            <Navbar/>
+            <Container forPage="jkk">
+                <div className="helper-box">
+                    <select></select>
+                    <select></select>
+                    <select></select>
+                    <input type="search" name="namakar" />
+                </div>
+                <div className="helper-button">
+                    <button></button>
+                    <button></button>
+                    <button></button>
+                </div>
+                <table>
+                    <tr>
+                        <th></th>
+                        <th></th>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                </table>
+            </Container>
+        </Background>
+    );
+}
+
+export default Jkk;
