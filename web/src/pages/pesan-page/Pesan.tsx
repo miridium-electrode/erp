9b84b0c5608693b81818eb16ecbc047d97920b65
miridium-electrode <checkerdance@yahoo.com>
2021-01-27 19:08:03 +0700

    added pesan page

---
+++ web/src/pages/pesan-page/Pesan.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import Background from '../../components/background/Background';
+import Navbar from '../../components/navbar/Navbar';
+import Container from '../../components/container/Container';
+
+function Pesan() {
+    return (
+        <Background forPage="other">
+            <Navbar/>
+            <Container forPage="pesan">
+                <div className="checkbox">
+                    
+                </div>
+                <div className="message">
+                    
+                </div>
+            </Container>
+        </Background>
+    );
+}
+
+export default Pesan;
