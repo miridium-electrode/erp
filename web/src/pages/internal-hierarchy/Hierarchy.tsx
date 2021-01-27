c1484a5dc1800d3cee3ef8344981d100660b095c
miridium-electrode <checkerdance@yahoo.com>
2021-01-27 19:07:07 +0700

    added stub hierarchy page

---
+++ web/src/pages/internal-hierarchy/Hierarchy.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Background from '../../components/background/Background';
+import Navbar from '../../components/navbar/Navbar';
+import Container from '../../components/container/Container';
+
+function Hierarchy() {
+    return (
+        <Background forPage="other">
+            <Navbar/>
+            <Container forPage="internalhierarchy">
+                <div className="field">
+                    <label></label>
+                    <input type="text" />
+                </div>
+            </Container>
+        </Background>
+    );
+}
+
+export default Hierarchy;
