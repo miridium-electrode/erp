81fce544ac373a5e805628a0f4051b103298608d
miridium-electrode <checkerdance@yahoo.com>
2021-01-27 19:07:40 +0700

    added stub satpam page

---
+++ web/src/pages/satpam-show-page/Show.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import Background from '../../components/background/Background';
+import Navbar from '../../components/navbar/Navbar';
+import Container from '../../components/container/Container';
+
+function Show() {
+    return (
+        <Background forPage="other">
+            <Navbar/>
+            <Container forPage="show">
+                <div className="select-menu"></div>
+                <table>
+                    <tr>
+                        <th>Nama</th>
+                        <th>Masuk</th>
+                        <th>Keluar</th>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                </table>
+            </Container>
+        </Background>
+    );
+}
+
+export default Show;
