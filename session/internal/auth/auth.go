e89e94cc6c6e55a368497a4a2558e58c6885bb18
miridium-electrode <checkerdance@yahoo.com>
2021-02-15 18:09:08 +0700

    change to basic auth

--- session/internal/auth/auth.go
+++ session/internal/auth/auth.go
@@ -3,25 +3,8 @@ package auth
 import (
 	"net/http"
 	"encoding/json"
-	"time"
 	"miridium_electrode/session/internal/dbquery"
-	jwt "github.com/dgrijalva/jwt-go"
 )
-
-// store jwt token and then converted to json
-type M map[string]interface{}
-
-// Representation of jwt data
-type MyClaim struct {
-	jwt.StandardClaims
-	ID string
-	Username string `json:"username"`
-}
-
-var APPLICATION_NAME = "session of erp"
-var LOGIN_EXPIRATION_DURATION = time.Duration(1) * time.Hour
-var JWT_SIGNING_METHOD = jwt.SigningMethodHS256
-var JWT_SIGNATURE_KEY = []byte("the secret of kalimdor")
 
 // Accept post request of username and password, and then return a cookie of json web token
 func Login(w http.ResponseWriter, r *http.Request) {
@@ -33,50 +16,31 @@ func Login(w http.ResponseWriter, r *http.Request) {
 		  w.Write([]byte("allowed"))
 		  return
 	  case "POST":
-		  decoder := json.NewDecoder(r.Body)
-		  payload := struct{
-			  Username string `json:"username"`
-			  Password string `json:"password"`
-		  }{}
-		  if err := decoder.Decode(&payload); err != nil {
-			  http.Error(w, err.Error(), http.StatusInternalServerError)
-			  return
-		  }
+		username, password, ok := r.BasicAuth()
 
-		ok := dbquery.DbAuthenticateUser(payload.Username, payload.Password)
-		  if !ok == true {
-			  http.Error(w, "invalid username or password", http.StatusBadRequest)
-			  return
-		  }
+		if !ok {
+			http.Error(w, "Invalid http header", http.StatusBadRequest)
+			return
+		}
 
-		id, exist := dbquery.DbGetID(payload.Username, payload.Password)
+		valid := dbquery.DbAuthenticateUser(username, password)
+
+		if !valid {
+			http.Error(w, "Invalid username or password", http.StatusBadRequest)
+			return
+		}
+
+		id, exist := dbquery.DbGetID(username, password)
 		if !exist == true {
 			http.Error(w, "invalid username or password", http.StatusBadRequest)
 			return
 		}
-		  claims := MyClaim {
-			  StandardClaims: jwt.StandardClaims{
-				  Issuer:    APPLICATION_NAME,
-				  ExpiresAt: time.Now().Add(LOGIN_EXPIRATION_DURATION).Unix(),
-			  },
-			  ID: id,
-			  Username: payload.Username,
-		  }
 
-		  token := jwt.NewWithClaims(
-			  JWT_SIGNING_METHOD,
-			  claims,
-		  )
-
-		  signedToken, tokerr := token.SignedString(JWT_SIGNATURE_KEY)
-		  if tokerr != nil {
-			  http.Error(w, tokerr.Error(), http.StatusBadRequest)
-			  return
-		  }
-
-		setCookie(w, r, signedToken)
-		var msg string = "welcome " + payload.Username
-		reserr := json.NewEncoder(w).Encode(M{"message": msg})
+		cookieComp := []string{id, username}
+		setCookie(w, r, cookieComp)
+		var msg string = "welcome " + username
+		mjson := map[string]interface{}{"message": msg}
+		reserr := json.NewEncoder(w).Encode(mjson)
 		if reserr != nil {
 			http.Error(w, reserr.Error(), http.StatusInternalServerError)
 		}
@@ -86,21 +50,18 @@ func Login(w http.ResponseWriter, r *http.Request) {
 }
 
 
-// set user cookie in form of jwt
-func setCookie(w http.ResponseWriter, r *http.Request, st string) {
-	cookieName := "token"
-	c := &http.Cookie{}
-
-	if storedCookie, _ := r.Cookie(cookieName); storedCookie != nil {
-		c = storedCookie
+// set user cookie
+func setCookie(w http.ResponseWriter, r *http.Request, st []string) {
+	c1 := &http.Cookie{
+		Name: "uid",
+		Value: st[0],
 	}
 
-	if c.Value == "" {
-		c := &http.Cookie{
-			Name: cookieName,
-			Value: st,
-			Expires: time.Now().Add(LOGIN_EXPIRATION_DURATION),
-		}
-		http.SetCookie(w, c)
+	c2 := &http.Cookie {
+		Name: "username",
+		Value: st[1],
 	}
+
+	http.SetCookie(w, c1)
+	http.SetCookie(w, c2)
 }
