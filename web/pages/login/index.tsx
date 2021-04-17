2861cd9716d70c67ba17a232d16e87ed254aa09d
miridium-electrode <checkerdance@yahoo.com>
2021-04-17 08:33:44 +0000

    uncolored login page

--- web/pages/login/index.tsx
+++ web/pages/login/index.tsx
@@ -6,16 +6,17 @@ function Login() {
     return (
         <Background>
             <Container forPage="sign-star">
+                <h2>Login</h2>
                 <div className={styles.field}>
                     <label htmlFor="username">Username</label>
                     <input type="text" name="username"/>
                 </div>
-                <div className={styles.fiels}>
+                <div className={styles.field}>
                     <label htmlFor="pwd">Password</label>
                     <input type="password" name="pwd"/>
                 </div>
-                <a href="#">Forgot Password?</a>
-                <a href="#">Not A User?</a>
+                <a href="#" className={styles.useful_links}>Forgot Password?</a>
+                <a href="#" className={styles.useful_links}>Not A User?</a>
                 <a href="#" className={styles.login_btn}>Login</a>
             </Container>
         </Background>
