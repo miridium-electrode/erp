2875126fe826e43dc7aad3325e2eaf5e1df81158
miridium-electrode <checkerdance@yahoo.com>
2021-04-17 07:33:24 +0000

    pseudo landing page created, removed home styles

--- web/pages/_app.tsx
+++ web/pages/_app.tsx
@@ -1,3 +1,5 @@
+import '@styles/globals.scss';
+
 function App({Component, pageProps}) {
     return <Component {...pageProps} />
 }
--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -1,21 +1,62 @@
 function LandingPage() {
     return (
-        <div className="LandingBg">
-            <nav>
-                <a href="#" id="login">Login</a>
-            </nav>
+        <div className="landing-bg">
+            <header>
+                <a href="#" className="logo">A+ERP</a>
+                <div className="pseudo-buttons">
+                    <a href="#">Sign In</a>
+                    <a href="#" className="sign-up">Try It</a>
+                </div>
+                <ul>
+                    <li>
+                        <a href="#">Features</a>
+                    </li>
+                    <li>
+                        <a href="#">Pricing</a>
+                    </li>
+                </ul>
+            </header>
 
             <style jsx>{`
-                #login {
-                    background-color: hsl(187, 62%, 53%);
-                    border: none;
-                    text-align: center;
-                    text-decoration: none;
-                    padding: 1rem .5rem;
-                    cursor: pointer;
-                    color: white;
-                    &:hover {
-                        background-color: hsl(198, 59%, 56%);
+                .landing-bg {
+                    header {
+                        background-color: black;
+                        width: 100vw;
+                        padding: 1rem;
+                        .logo {
+                            color: white;
+                            float: left;
+                        }
+                        .pseudo-buttons {
+                            float: right;
+                            a {
+                                color: white;
+                                padding: .5rem 1rem;
+                            }
+                            .sign-up {
+                                background-color: hsl(187, 62%, 53%);
+                                border: none;
+                                text-align: center;
+                                text-decoration: none;
+                                cursor: pointer;
+                                color: white;
+                                &:hover {
+                                    background-color: hsl(198, 59%, 56%);
+                                }
+                            }
+                        }
+                        ul {
+                            margin: 0 auto;
+                            padding: 0;
+                            text-align: center;
+                            li {
+                                color: white;
+                                margin: {
+                                    left: .5rem;
+                                    right: .5rem;
+                                } 
+                            }
+                        }
                     }
                 }
             `}</style>
