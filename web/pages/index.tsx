b41a17113f2d88046531c7a8015b1b8632f24a50
miridium-electrode <checkerdance@yahoo.com>
2021-04-18 08:54:13 +0000

    seperate css with jsx, because it might get big

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -1,11 +1,13 @@
+import styles from "@styles/landing-page.module.scss";
+
 function LandingPage() {
     return (
-        <div className="landing-bg">
+        <div className={styles["landing-bg"]}>
             <header>
-                <a href="#" className="logo">A+ERP</a>
-                <div className="pseudo-buttons">
+                <a href="#" className={styles["logo"]}>A+ERP</a>
+                <div className={styles["pseudo-buttons"]}>
                     <a href="/login">Sign In</a>
-                    <a href="/signup" className="sign-up">Try It</a>
+                    <a href="/signup" className={styles["sign-up"]}>Try It</a>
                 </div>
                 <ul>
                     <li>
@@ -16,50 +18,6 @@ function LandingPage() {
                     </li>
                 </ul>
             </header>
-
-            <style jsx>{`
-                .landing-bg {
-                    header {
-                        background-color: black;
-                        width: 100vw;
-                        padding: 1rem;
-                        .logo {
-                            color: white;
-                            float: left;
-                        }
-                        .pseudo-buttons {
-                            float: right;
-                            a {
-                                color: white;
-                                padding: .5rem 1rem;
-                            }
-                            .sign-up {
-                                background-color: hsl(187, 62%, 53%);
-                                border: none;
-                                text-align: center;
-                                text-decoration: none;
-                                cursor: pointer;
-                                color: white;
-                                &:hover {
-                                    background-color: hsl(198, 59%, 56%);
-                                }
-                            }
-                        }
-                        ul {
-                            margin: 0 auto;
-                            padding: 0;
-                            text-align: center;
-                            li {
-                                color: white;
-                                margin: {
-                                    left: .5rem;
-                                    right: .5rem;
-                                } 
-                            }
-                        }
-                    }
-                }
-            `}</style>
         </div>
     );
 }
