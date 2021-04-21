8fb746e1f972dc8e5ab1c7c60fcbbc9e5cd2eee9
miridium-electrode <checkerdance@yahoo.com>
2021-04-21 11:02:07 +0000

    info page

---
+++ web/pages/employees/profiles/info.tsx
@@ -0,0 +1,52 @@
+import Background from "@components/background";
+import Container from "@components/container/profiles";
+import Navbar from "@components/navbar/employees/attendance/employee";
+
+
+function Left() {
+    return (
+        <>
+            <img src="" alt="Profile Picture"/>
+            <a href="/employees/profiles/info">Profile Info</a>
+            <a href="/employees/profiles/settings">User Settings</a>
+        </>
+    );
+}
+
+function Right() {
+    return (
+        <>
+            <div className="field">
+                <label htmlFor="nama">nama</label>
+                <input type="text"/>
+            </div>
+            <div className="field">
+                <label htmlFor="alamat">alamat</label>
+                <input type="text"/>
+            </div>
+            <div className="field">
+                <label htmlFor="status">status</label>
+                <input type="text"/>
+            </div>
+            <div className="field">
+                <label htmlFor="jabatan">jabatan</label>
+                <input type="text"/>
+            </div>
+            <div className="field">
+                <label htmlFor="tgl">Tanggal Lahir</label>
+                <input type="text" name="tgl"/>
+            </div>
+        </>
+    );
+}
+
+function Info() {
+    return (
+        <Background>
+            <Navbar />
+            <Container left={<Left />} right={<Right />} />
+        </Background>
+    );
+}
+
+export default Info;
