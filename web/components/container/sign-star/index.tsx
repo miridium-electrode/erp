8de5a082be990708bd0d8a50c6f20583ce5fb391
miridium-electrode <checkerdance@yahoo.com>
2021-04-28 10:49:02 +0000

    docs sign-* page

--- web/components/container/sign-star/index.tsx
+++ web/components/container/sign-star/index.tsx
@@ -4,6 +4,12 @@ interface ContainerProp extends BasicProp {
     forPage?: string;
 }
 
+/**
+ * Container Components for Sign-Up and Sign-in page
+ * @param props 
+ * take an option strings and Component
+ * @returns JSX.Elements
+ */
 function Container(props: ContainerProp) {
     return (
         <div>
