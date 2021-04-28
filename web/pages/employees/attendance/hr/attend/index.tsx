5e6ee7cc13b3a2c3eb97e59b4fc3275df1a57f85
miridium-electrode <checkerdance@yahoo.com>
2021-04-28 09:36:54 +0000

    add hr attendance page

---
+++ web/pages/employees/attendance/hr/attend/index.tsx
@@ -0,0 +1,33 @@
+import Background from "@components/background";
+import Container from "@components/container/employees/attendance/employee/attend";
+import styles from "./styles.module.scss";
+import Navbar from "@components/navbar/employees/attendance/employee";
+
+function Attend() {
+    return (
+        <Background>
+            <Navbar/>
+            <Container>
+                <div className={styles.part}>
+                    <button>
+                        Masuk
+                    </button>
+                    <button>
+                        Keluar
+                    </button>
+                </div>
+                <div className={`${styles.part} ${styles.inputs}`}>
+                    <input type="text" placeholder="About"/>
+                    <select>
+                        <option value="sick">Sick</option>
+                        <option value="permit">Permit</option>
+                    </select>
+                    <textarea placeholder="permit reason"></textarea>
+                    <input type="file" name="attachment" placeholder="attachment"/>
+                </div>
+            </Container>
+        </Background>
+    );
+}
+
+export default Attend;
