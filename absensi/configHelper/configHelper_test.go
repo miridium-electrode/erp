1b500feac2a4f11326f7ed2bc4ca528b21b01c06
miridium-electrode <checkerdance@yahoo.com>
2021-07-05 12:09:16 +0000

    config helper

---
+++ absensi/configHelper/configHelper_test.go
@@ -0,0 +1,22 @@
+package configHelper
+
+import (
+	"testing"
+)
+
+func TestInitConfig(t *testing.T) {
+	const (
+		dbdriver = "sqlite"
+		dbpath   = "/home/git/erp/absensi"
+	)
+	envData, err := InitConfig(".", "..")
+	if err != nil {
+		t.Fatalf("Error while umarshalling value: %s", err.Error())
+	}
+	if envData.DBDriver != dbdriver {
+		t.Errorf("expected dbdriver: %s, got %s\n", dbdriver, envData.DBDriver)
+	}
+	if envData.DBPath != dbpath {
+		t.Errorf("expected dbpath: %s, got %s\n", dbpath, envData.DBPath)
+	}
+}
